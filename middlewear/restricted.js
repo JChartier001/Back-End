d05d85f5bf604138ae75993da97ddb9c4d1a3592
Adam <adamcpenman@gmail.com>
2020-01-29 19:03:06 -0600

    authenticate

--- middlewear/restricted.js
+++ middlewear/restricted.js
@@ -18,24 +18,3 @@ module.exports = (req, res, next) => {
   }
 
 }
-
-// function authenticate (req, res, next) {
-//     const token = req.headers.authorization;
-
-//     if (token) {
-//         jwt.verify(token, secret, (err, decoded) => {
-//             if (err) {
-//                 return res.status(401).json(err)
-//             } else {
-//                 req.decoded = decoded;
-//                 next();
-//             }
-//         });
-//     } else {
-//         return res.status(401).json({
-//             error: "No token provided, must be set on the Authorization Header"
-//         })
-//     }
-// }
-
-// module.exports = authenticate;
