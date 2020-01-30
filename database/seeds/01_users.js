5f5b6f130509da97d916c57b914e8060156fc134
Adam <adamcpenman@gmail.com>
2020-01-29 19:36:51 -0600

    seed data

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -6,6 +6,7 @@ exports.seed = async (knex) => {
     await knex('users').truncate()
 
     await knex('users').insert([
-        { firstname: "Harry", lastName:"Potter", email: "Harry@potter.com", password: `${await hash('testing1')}`}
+        { firstname: "Harry", lastName:"Potter", email: "Harry@potter.com", password: `${await hash('testing1')}`},
+        { firstname: "Sirius", lastName:"Black", email: "Black@potter.com", password: `${await hash('testing1')}`}
      ])
 }
